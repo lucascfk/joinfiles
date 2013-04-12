9716f96b8b451152c5c28f74a47dfef7e674c0ea
Lucas Emanoel <lucascfk@outlook.com>
2013-04-12 02:02:22 -0300

    organizando arquivos com um contador de 1 a 999.999

--- joinfiles.py
+++ joinfiles.py
@@ -1,2 +1,47 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
+
+from os import listdir, path, mkdir
+from shutil import copy2
+
+
+def directoryScan(local_path):
+	directory = listdir(local_path)
+	directory.sort()
+
+	folders = []
+	files = []
+	for d in directory:
+		if not any(local_path+'/'+d in ignore for ignore in ignore_list):
+			if path.isdir(local_path+'/'+d):
+				folders.append(d)
+			elif path.isfile(local_path+'/'+d):
+				files.append(d)
+
+	return {'folders': folders, 'files': files}
+
+
+def appendFiles(local_path):
+	scan_result = directoryScan(local_path)
+	global count
+
+	for f in scan_result['files']:
+		origin = local_path+'/'+f
+		extension = path.splitext(origin)[1]
+		target = './joined_files/%s%s' % (('000000'+str(count))[-6:], extension)
+		
+		copy2(origin, target)
+		count += 1
+
+
+	for f in scan_result['folders']:
+		appendFiles(f)
+
+
+ignore_list = ['./joined_files', './joinfiles.py']
+
+count = 0
+
+mkdir('joined_files')
+
+appendFiles('.')
